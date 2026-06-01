b6242f2cd9f52a60890abb3a571f7d1d8e42536d
Vansh garg <vanshgarg08080@gmail.com>
2026-06-01 18:42:10 +0530

    Implement stock buying and selling profit calculation in c++

---
+++ Week_1/Day_1/Best_Time_to_Buy_and_Sell_Stock.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int n = prices.size();
+        int minPrice = prices[0];
+        int maxProfit = 0;
+        for(int i = 1; i < n; i++) {
+            maxProfit = max(maxProfit, prices[i] - minPrice);
+            minPrice = min(minPrice, prices[i]);
+        }
+        return maxProfit;
+    }
+};
