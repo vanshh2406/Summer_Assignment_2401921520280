acfd9d8c131d407d4d59d2b98f7ca3a5b1d43c09
Vansh garg <vanshgarg08080@gmail.com>
2026-06-01 18:16:30 +0530

    Implement Two Sum solution in C++
    Time complexity of this solution = O(n)
    Space complexity of this solution = O(n)

---
+++ Week_1/Day_1/Two_Sum.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        vector<int> ans;
+        unordered_map<int,int> m;
+        for(int i=0;i<nums.size();i++){
+            int rem = target - nums[i];
+            if(m.find(rem)!=m.end()){
+                ans.push_back(m[rem]);
+                ans.push_back(i);
+            }
+            else m[nums[i]] = i;
+        }
+        return ans;
+    }
+};
