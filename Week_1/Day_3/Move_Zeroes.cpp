316fa76386423100eb04971538b672fa681aeb7a
Vansh garg <vanshgarg08080@gmail.com>
2026-06-03 21:21:13 +0530

    Implement moveZeroes function to rearrange array in c++

---
+++ Week_1/Day_3/Move_Zeroes.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        int n = nums.size();
+        int k = 0;
+        for(int i=0;i<n;i++){
+            if(nums[i]!=0){
+                int temp = nums[k];
+                nums[k] = nums[i];
+                nums[i] = temp;
+                k++;
+            }
+        }
+    }
+};
