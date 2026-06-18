db2215348425edefc4d827f93e99b1442e1df9b2
Vansh garg <vanshgarg08080@gmail.com>
2026-06-18 12:14:04 +0530

    Add Daily Temperatures solution class
    Implement dailyTemperatures function to calculate days until warmer temperatures.

---
+++ Week_3/Day_4/Daily_Temperatures.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    vector<int> dailyTemperatures(vector<int>& temperatures) {
+        stack<int> st;
+        int n = temperatures.size();
+        vector<int> v(n,0);
+        for(int i=n-1;i>=0;i--){
+            while(st.size()>0 && temperatures[st.top()]<=temperatures[i]) {
+                st.pop();
+            }
+            if(st.size()>0) v[i] = st.top()-i;
+            st.push(i);
+        }
+        return v;
+    }
+};
