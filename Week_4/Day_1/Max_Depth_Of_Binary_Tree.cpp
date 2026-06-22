dc830e33d40d64b24259166e0a5165f90a4e386d
Vansh garg <vanshgarg08080@gmail.com>
2026-06-22 21:57:58 +0530

    Implement maxDepth function for binary tree

---
+++ Week_4/Day_1/Max_Depth_Of_Binary_Tree.cpp
@@ -0,0 +1,9 @@
+class Solution {
+public:
+    int maxDepth(TreeNode* root) {
+        if(root == NULL) return 0;
+        int ld = maxDepth(root->left);
+        int rd = maxDepth(root->right);
+        return 1 + max(ld, rd);
+    }
+};
