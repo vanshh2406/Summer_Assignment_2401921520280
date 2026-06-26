2ccbb2e065af5c8ff0215aa2aeae8393159daf22
Vansh garg <vanshgarg08080@gmail.com>
2026-06-26 12:33:21 +0530

    Implement binary tree construction from preorder and inorder

---
+++ Week_4/Day_5/Construct_Binary_Tree_From_Pre_In_Traversal.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    TreeNode* build(vector<int>& pre, int prelo, int prehi, vector<int>& in, int inlo, int inhi){
+        if(prelo>prehi) return NULL;
+        TreeNode* root = new TreeNode(pre[prelo]);
+        if(prelo==prehi) return root;
+        int i = inlo;
+        while(i<=inhi){
+            if(in[i]==pre[prelo]) break;
+            i++;
+        }
+        int leftcount = i-inlo;
+        int rightcount = inhi-i;
+        root->left = build(pre, prelo+1, prelo+leftcount, in, inlo, i-1);
+        root->right = build(pre, prelo+leftcount+1, prehi, in, i+1, inhi);
+        return root;
+    }
+    TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder){
+        int n = preorder.size();
+        return build(preorder,0,n-1,inorder,0,n-1);
+    }
+}; 
