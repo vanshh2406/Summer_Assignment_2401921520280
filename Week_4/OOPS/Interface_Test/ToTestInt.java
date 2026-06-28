ea4806eec610a82439da018423990cf3bec34e9c
Vansh garg <vanshgarg08080@gmail.com>
2026-06-28 22:14:56 +0530

    Implement interface for square calculation

---
+++ Week_4/OOPS/Interface_Test/ToTestInt.java
@@ -0,0 +1,18 @@
+interface Test {
+    int square(int n);
+}
+
+class Arithmetic implements Test {
+    public int square(int n) {
+        return n * n;
+    }
+}
+
+public class ToTestInt {
+    public static void main(String[] args) {
+        Arithmetic obj = new Arithmetic();
+
+        int num = 5;
+        System.out.println("Square of " + num + " = " + obj.square(num));
+    }
+}
