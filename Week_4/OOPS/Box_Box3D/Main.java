7549e810f176c28f29c0507be4e53b6a00ae58b4
Vansh garg <vanshgarg08080@gmail.com>
2026-06-28 22:17:54 +0530

    Implement Box and Box3D classes with area and volume

---
+++ Week_4/OOPS/Box_Box3D/Main.java
@@ -0,0 +1,28 @@
+class Box {
+    double length;
+    double breadth;
+    Box(double length, double breadth) {
+        this.length = length;
+        this.breadth = breadth;
+    }
+    double area() {
+        return length * breadth;
+    }
+}
+class Box3D extends Box {
+    double height;
+    Box3D(double length, double breadth, double height) {
+        super(length, breadth);
+        this.height = height;
+    }
+    double volume() {
+        return length * breadth * height;
+    }
+}
+public class Main {
+    public static void main(String[] args) {
+        Box3D box = new Box3D(5, 4, 3);
+        System.out.println("Area = " + box.area());
+        System.out.println("Volume = " + box.volume());
+    }
+}
